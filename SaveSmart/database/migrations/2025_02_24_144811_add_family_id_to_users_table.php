a4624dff31fd102646f78a9a50c4215af174b38e
JIHANE ELMAJDI <emleyamanda9@gmail.com>
2025-02-24 14:59:49 +0000

    Modifier le fichier de migration

--- SaveSmart/database/migrations/2025_02_24_144811_add_family_id_to_users_table.php
+++ SaveSmart/database/migrations/2025_02_24_144811_add_family_id_to_users_table.php
@@ -4,25 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->foreignId('family_id')->nullable()->constrained('families')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropForeign(['family_id']);
+            $table->dropColumn('family_id');
         });
     }
 };
