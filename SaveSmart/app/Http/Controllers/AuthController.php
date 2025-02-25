e8c26b683e31242f07e421faf2b48fd6ecf435fe
JIHANE ELMAJDI <emleyamanda9@gmail.com>
2025-02-25 15:47:53 +0000

    version 1.0 connexion et register

--- SaveSmart/app/Http/Controllers/AuthController.php
+++ SaveSmart/app/Http/Controllers/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
     ]);
 
     if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-        return redirect()->route('dashboard');
+        return redirect()->route('dashbord');
     } else {
         return back()->with('error', 'Email ou mot de passe incorrecte ');
     }
