7ed28b4e4222ce07381cd5b277d449bd0c24450a
JIHANE ELMAJDI <emleyamanda9@gmail.com>
2025-02-25 10:30:00 +0000

    Refonte complète du frontend : mise à jour de la page d'accueil (home) et ajout des interfaces de connexion (login) et d'inscription (register)

--- SaveSmart/resources/views/home.blade.php
+++ SaveSmart/resources/views/home.blade.php
@@ -28,10 +28,10 @@
         </ul>
         <div class="flex-grow"></div>
         <div class="hidden items-center justify-center gap-6 md:flex">
-            <a href="#" class="font-dm text-sm font-medium text-slate-700">Sign in</a>
+            <a href="{{route('login')}}" class="font-dm text-sm font-medium text-slate-700">Se connecter</a>
             <a href="#"
-                class="rounded-md bg-gradient-to-br from-green-600 to-emerald-400 px-3 py-1.5 font-dm text-sm font-medium text-white shadow-md shadow-green-400/50 transition-transform duration-200 ease-in-out hover:scale-[1.03]">Sign
-                up for free
+                class="rounded-md bg-gradient-to-br from-green-600 to-emerald-400 px-3 py-1.5 font-dm text-sm font-medium text-white shadow-md shadow-green-400/50 transition-transform duration-200 ease-in-out hover:scale-[1.03]">
+                s'inscrire
             </a>
         </div>
         <div class="relative flex items-center justify-center md:hidden">
